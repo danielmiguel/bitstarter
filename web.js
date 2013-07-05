2f616bb1b745aa80676ec88944cf8fe4dd4f944b
Daniel Miguel <danielmp84@gmail.com>
2013-07-05 04:12:20 +0000

    web.js modified

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 var express = require('express');
 
+function getContentFromFile(filename){
+
+
+    var fs = require('fs');
+    fs.readFileSync(filename, function(err, data){
+	if(err)
+	    return "error reading the file";
+	return "data";
+    }
+	return "index";
+
+}
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(getContentFromFile('index.html'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
