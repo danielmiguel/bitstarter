429af7259adab229c0ce73c5d2f5acb9091543ed
Daniel Miguel <danielmp84@gmail.com>
2013-07-05 04:31:08 +0000

    testing

--- web.js
+++ web.js
@@ -2,20 +2,14 @@ var express = require('express');
 
 function getContentFromFile(filename){
 
-
-    var fs = require('fs');
-    fs.readFileSync(filename, function(err, data){
-	if(err)
-	    return "error reading the file";
-	return "data";
-    }
-	return "index";
+return filename + " file";
 
 }
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(getContentFromFile('index.html'));
+  response.send(getContentFromFile('index'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
